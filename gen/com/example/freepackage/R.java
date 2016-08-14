9ad2dff31f8e633f5b7c04e45d57e2fa23b84d20
Gbq <990924291@qq.com>
2016-08-14 19:45:05 +0800

    8.14

--- gen/com/example/freepackage/R.java
+++ gen/com/example/freepackage/R.java
@@ -25,10 +25,7 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090003;
-        public static final int myButton1=0x7f090001;
-        public static final int myEditText1=0x7f090002;
-        public static final int myTextView1=0x7f090000;
+        public static final int action_settings=0x7f090000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -42,6 +39,7 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f060002;
         public static final int app_name=0x7f060000;
+        public static final int button_name=0x7f060003;
         public static final int hello_world=0x7f060001;
     }
     public static final class style {
